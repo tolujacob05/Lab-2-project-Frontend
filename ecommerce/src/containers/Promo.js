30c7c1bff8353b53822965d5228974406a787549
Olusoga Daniel <Olusogadaniel001@gmail.com>
2022-10-24 16:29:44 +0100

    CART DESIGN AND IMAGE OVERFLOW

--- ecommerce/src/containers/Promo.js
+++ ecommerce/src/containers/Promo.js
@@ -2,11 +2,40 @@ import React from "react"
 import Autum from "./Pictures/Autum.jpg"
 import styles from "./Promo.module.css"
 
+
+
+
 const Promo = () => {
     return (
-        <div className={styles.autum}>
-            <img src={Autum} alt="this is an autum promo" />
+        <div className={styles.parent}>
+            
+                <div className={styles.pc1}>
+                 
+                    Shopping Experience <br/>
+                    made easier
+                 <button> Find store nearby</button>
+                 <button> Saved Stores</button>
+                </div>
+
+                {/* <div className={styles.pc2}>
+               <div className={styles.suh}>
+               <img src={Autum} alt="this is an autum promo" height={"400"} width={"400"}  /> 
+               </div>
+
+               
+                
+                </div> */}
+
+                <div className={styles.pc2}>
+               
+                </div>
+              
+                
+                
+
         </div>
-    )
+        
+    );
 }
+
 export default Promo;
